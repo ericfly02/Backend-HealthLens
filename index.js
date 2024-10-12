92e8fb2a4311f03b9117929a42ea6ff198bc0fb3
EricGd <speedyand@gmail.com>
2024-10-12 19:44:35 +0200

    speech to text removed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const imageRoutes = require('./routes/image');
 const chatRoutes = require('./routes/chat');
 const userRoutes = require('./routes/userRoutes');
 const mapRoutes = require('./routes/map');
-const speechToTextRoutes = require('./routes/speechToTextRoutes');
+//const speechToTextRoutes = require('./routes/speechToTextRoutes');
 
 // Initialize Express app
 const app = express();
@@ -24,7 +24,7 @@ app.use('/upload-image', imageRoutes);
 app.use('/chat', chatRoutes);
 app.use('/user', userRoutes);
 app.use('/map', mapRoutes);
-app.use('/speech', speechToTextRoutes);
+//app.use('/speech', speechToTextRoutes);
 
 // Export the app (remove app.listen)
 module.exports = app;
--- middlewares/upload.js
+++
@@ -1,9 +0,0 @@
-const multer = require('multer');
-
-// Set up multer in-memory storage
-const storage = multer.memoryStorage();
-
-// Create a multer instance with the in-memory storage
-const upload = multer({ storage: storage });
-
-module.exports = upload;
