c2311975590c698acd715e327f4f681cacd87e7e
EricGd <speedyand@gmail.com>
2024-10-12 19:38:26 +0200

    Update upload.js

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,18 +1,9 @@
 const multer = require('multer');
-const path = require('path');
 
-// Set up multer storage
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'uploads/'); // Directory where files are saved
-  },
-  filename: (req, file, cb) => {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-    cb(null, `${uniqueSuffix}-${file.originalname}`);
-  },
-});
+// Set up multer in-memory storage
+const storage = multer.memoryStorage();
 
-// Create a multer instance with the defined storage
+// Create a multer instance with the in-memory storage
 const upload = multer({ storage: storage });
 
 module.exports = upload;
