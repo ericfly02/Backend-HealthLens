39c01c9690c19f9b9cd251517e64e25cbcc6cdf5
EricGd <speedyand@gmail.com>
2024-10-13 18:34:32 +0200

    Update chat.js

--- routes/chat.js
+++ routes/chat.js
@@ -3,8 +3,6 @@ const router = express.Router();
 const { startConversation } = require('../controllers/chatController');
 const authMiddleware = require('../middlewares/authMiddleware');
 
-router.post('/', authMiddleware, chat);
-
 // Route for initiating Watson conversation after image analysis
 router.post('/start-conversation', startConversation);
 
